0c3bd7e226279b775554a3d12ce1eaf6f0c02201
ellamary <ellamary@gmail.com>
2017-06-29 17:29:46 +0000

    Add account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -59,8 +59,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
-  
-  test "should redirect destroy when logged in as non-admin" do
+
+  test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
       delete user_path(@user)
